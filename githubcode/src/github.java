835cdb85812f7e11563bc461b40a48df4e3c8101
PrincessJaydeofWarri <61886572+PrincessJaydeofWarri@users.noreply.github.com>
2020-04-21 19:46:41 +0100

    code added

---
+++ githubcode/src/github.java
@@ -0,0 +1,11 @@
+public class github  {
+
+    public static void main (String[] args) {
+
+        int num = 5;
+        double doubley = 1.6;
+        char firstIntial = 'J';
+        String name = "Jayde";
+
+    }
+}
